ca850a23160f25f72a5633c07fd8ad0ea3ee07d7
Fabricejor <Fabricejordan2001@gmail.com>
2024-02-05 20:35:08 +0000

    final commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
 import Navbar from './components/Navbar';
@@ -7,7 +7,10 @@ import "./assets/style/Filter.css"
 
 
 const App = () => {
-  // const [show,setShow] = useState(false);
+  const [show,setShow] = useState(false);
+  const toggleShow =()=>{
+    setShow(!show)
+  }
   // les etats pour chaque
   const [movies, setMovies] = useState([
     { titre: 'Code and Furious', description: 'Action , Course ,Drogue ,Adrénaline, Adventure', posterURL: 'https://static.posters.cz/image/750webp/114155.webp', note: 9.8 },
@@ -19,82 +22,82 @@ const App = () => {
   ]);
 
   //! initialisation des donnés filtres 
-  const [titre , setTitre]= useState("");//chaine
-  const [note , setNote]= useState(0);//nombre 
+  const [titre, setTitre] = useState("");//chaine
+  const [note, setNote] = useState(0);//nombre 
   //!!!!! Creation de nouveaux etats pour l'ajouts il ne doivent pas partager le meme noms
   const [addtitre, setAddtitre] = useState("");//chaine
   const [addresume, setAddresume] = useState("");
-  const [addnote , setAddnote] = useState(0);//number
-  const [addurl , setAddurl] = useState("");
+  const [addnote, setAddnote] = useState(0);//number
+  const [addurl, setAddurl] = useState("");
   //! fonction de filtre
   const filteredMovies = movies.filter(movie =>
-  (titre === "" || (typeof movie.titre === 'string' && movie.titre.toLowerCase().includes(titre.toLowerCase())))
-  &&
-    (note === "" ||   movie.note >= parseFloat(note))
+    (titre === "" || (typeof movie.titre === 'string' && movie.titre.toLowerCase().includes(titre.toLowerCase())))
+    &&
+    (note === "" || movie.note >= parseFloat(note))
   );
-  
-console.log( "note "+note , "titre : "+titre);
-    //?pour récuperer toutes les donnés du 2nd formulaire 
-    const AddMovie = () => {
-      if (addtitre !="" && addnote !=0	) {
-        const newMovie = {
-          titre: addtitre,
-          description: addresume,
-          posterURL: addurl,
-          note: parseFloat(addnote)
-        };
-        //on ajoute a la liste le tableau d'objet NewMovies avec le spread operator
-        setMovies([...movies, newMovie])
+
+  console.log("note " + note, "titre : " + titre);
+  //?pour récuperer toutes les donnés du 2nd formulaire 
+  const AddMovie = () => {
+    // on ajoute que si les champs des formulaires sont remplis
+    if (addtitre != "" && addnote != 0) {
+      const newMovie = {
+        titre: addtitre,
+        description: addresume,
+        posterURL: addurl,
+        note: parseFloat(addnote)
       };
-      //! Reinitialise tous les donnéess du formulaire
-      setAddnote(0);
-      setAddtitre("");
-      setAddresume("");
-      setAddurl("");
+      //on ajoute a la liste le tableau d'objet NewMovies avec le spread operator
+      setMovies([...movies, newMovie])
+    };
+    //! Reinitialise tous les donnéess du formulaire
+    setAddnote(0);
+    setAddtitre("");
+    setAddresume("");
+    setAddurl("");
+    setShow(false)
   };
-    
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
       <div className="d-flex justify-content-center h-100">
-                <div className="search">
-                    <input type="text" className="search-input" placeholder="titre search" name="titre"
-                    value={titre} onChange={e => setTitre(e.target.value)}/>
-                    {/* value donne la valeur direct de la variable deriere le input 
+        <div className="search">
+          <input type="text" className="search-input" placeholder="titre search" name="titre"
+            value={titre} onChange={e => setTitre(e.target.value)} />
+          {/* value donne la valeur direct de la variable deriere le input 
                         onChange methode en javascript pour modifier l'etat avec le mot clé setTitre */}
-                        <a href="#" className="search-icon">
-                            <i className="fa fa-search"></i>
-                        </a>
-                </div>
-                <div className="search">
-                    <input type="number" className="search-input" placeholder="note" name="note"
-                    value={note} onChange={e => setNote(e.target.value)} />
-                        <a href="#" className="search-icon">
-                            <i className="fa fa-star"></i>
-                        </a>
-                </div>
-            </div>
-
-            <div className="add-form">
+          <a href="#" className="search-icon">
+            <i className="fa fa-search"></i>
+          </a>
+        </div>
+        <div className="search">
+          <input type="number" className="search-input" placeholder="note" name="note"
+            value={note} onChange={e => setNote(e.target.value)} />
+          <a href="#" className="search-icon">
+            <i className="fa fa-star"></i>
+          </a>
+        </div>
+      </div>
+          {!show &&
+          <div className='addbutton'>
+            <button onClick={toggleShow} title='ajouter film'> <i className="fa fa-plus-square"></i></button>
+            </div>}
+      { show &&
+        <div className="add-form">
           <h2>Ajouter un nouveau film</h2>
-          <form>
-            <label>Titre:</label>
-            <input type="text" value={addtitre} onChange={(e) => setAddtitre(e.target.value)} />
-
-            <label>Description:</label>
-            <input type="text" value={addresume} onChange={(e) => setAddresume(e.target.value)} />
-
-            <label>URL du Poster (image du film):</label>
-            <input type="text" value={addurl} onChange={(e) => setAddurl(e.target.value)} />
-
-            <label>Note:</label>
-            <input type="number" value={addnote} onChange={(e) => setAddnote(e.target.value)} />
+          <form className='form-add'>
+            <input type="text" placeholder="add titre" value={addtitre} onChange={(e) => setAddtitre(e.target.value)} />
+            <input type="text" placeholder='Ajouter resume ou description' value={addresume} onChange={(e) => setAddresume(e.target.value)} />
+            <input type="text" placeholder="lien vers le poster du film" value={addurl} onChange={(e) => setAddurl(e.target.value)} />
+            <input type="number" value={addnote} max={10} min={0} onChange={(e) => setAddnote(e.target.value)} />
 
             <button type="button" onClick={AddMovie}>Ajouter</button>
           </form>
         </div>
-            <MovieList movies={filteredMovies} />
-      
+      }
+      <MovieList movies={filteredMovies} />
+
     </div>
   );
 }
